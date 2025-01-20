11ad3dea84e3e2767f6e09f4221752cac5a53ec0
YagizErdemir06 <erdemiryagiz@proton.me>
2025-01-20 20:42:53 +0530

    rubyx: Drop HotWordEnrollment
    Signed-off-by: YagizErdemir06 <erdemiryagiz@proton.me>

--- device.mk
+++ device.mk
@@ -233,10 +233,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_nfc/android.hardware.nfc.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
-    $(LOCAL_PATH)/configs/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml
-
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore.km41.xml
 
